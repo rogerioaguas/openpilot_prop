def24d5919267d8d662c09763fca01791acccd7b
alfhern <hz.alfonso@gmail.com>
2022-02-21 11:10:50 +0100

    OSM Local server setup and support when installed.

--- selfdrive/manager/custom_dep.py
+++ selfdrive/manager/custom_dep.py
@@ -3,10 +3,11 @@ import os
 import sys
 import errno
 import shutil
-from urllib.request import urlopen
 from glob import glob
 import subprocess
 import importlib.util
+from urllib.request import urlopen
+
 
 # NOTE: Do NOT import anything here that needs be built (e.g. params)
 from common.basedir import BASEDIR
@@ -21,13 +22,15 @@ TMP_DIR = '/data/tmp'
 PYEXTRA_DIR = '/data/openpilot/pyextra'
 
 
-def wait_for_internet_connection():
+def wait_for_internet_connection(return_on_failure=False):
   while True:
     try:
       _ = urlopen('https://www.google.com/', timeout=10)
-      return
-    except Exception:
-      pass
+      return True
+    except Exception as e:
+      print(f'Wait for internet failed: {e}')
+      if return_on_failure:
+        return False
 
 
 def install_dep(spinner):
