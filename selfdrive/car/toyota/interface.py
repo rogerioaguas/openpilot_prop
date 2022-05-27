2d0d85c5730e0cc9a826de9ab4047713825bb6ad
AlexandreSato <66435071+AlexandreSato@users.noreply.github.com>
2022-05-27 20:41:18 -0300

    add a wheelSpeedFactor to the Prius_TSS2

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -235,6 +235,7 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.6371   # hand-tune
       ret.mass = 3115. * CV.LB_TO_KG + STD_CARGO_KG
       set_lat_tune(ret.lateralTuning, LatTunes.PID_N)
+      ret.wheelSpeedFactor = 1.05
 
     elif candidate == CAR.MIRAI:
       stop_and_go = True
