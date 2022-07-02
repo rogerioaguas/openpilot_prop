719b8ce4e9af4d67d16709efee5d0f6e1fbfee16
Bruno Colombini <bruno.colombini@me.com>
2022-07-02 17:47:35 -0300

    remove hotspot

--- selfdrive/ui/main.cc
+++ selfdrive/ui/main.cc
@@ -25,40 +25,40 @@ int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
   MainWindow w;
   setMainWindow(&w);
-  addTetheringConnection();
+//  addTetheringConnection();
   a.installEventFilter(&w);
   return a.exec();
 }
 
 
-void addTetheringConnection() {
-  Connection connection;
-  connection["connection"]["id"] = "Hotspot";
-  connection["connection"]["uuid"] = QUuid::createUuid().toString().remove('{').remove('}');
-  connection["connection"]["type"] = "802-11-wireless";
-  connection["connection"]["interface-name"] = "wlan0";
-  connection["connection"]["autoconnect"] = false;
-
-  connection["802-11-wireless"]["band"] = "bg";
-  connection["802-11-wireless"]["mode"] = "ap";
-  connection["802-11-wireless"]["ssid"] = tethering_ssid.toUtf8();
-
-  connection["802-11-wireless-security"]["group"] = QStringList("ccmp");
-  connection["802-11-wireless-security"]["key-mgmt"] = "wpa-psk";
-  connection["802-11-wireless-security"]["pairwise"] = QStringList("ccmp");
-  connection["802-11-wireless-security"]["proto"] = QStringList("rsn");
-  connection["802-11-wireless-security"]["psk"] = defaultTetheringPassword;
-
-  connection["ipv4"]["method"] = "shared";
-  QMap<QString,QVariant> address;
-  address["address"] = "192.168.43.1";
-  address["prefix"] = 24u;
-  connection["ipv4"]["address-data"] = QVariant::fromValue(IpConfig() << address);
-  connection["ipv4"]["gateway"] = "192.168.43.1";
-  connection["ipv4"]["route-metric"] = 1100;
-  connection["ipv6"]["method"] = "ignore";
-
-  QDBusInterface nm_settings(NM_DBUS_SERVICE, NM_DBUS_PATH_SETTINGS, NM_DBUS_INTERFACE_SETTINGS, bus);
-  nm_settings.setTimeout(DBUS_TIMEOUT);
-  nm_settings.call("AddConnection", QVariant::fromValue(connection));
-}
+//void addTetheringConnection() {
+//  Connection connection;
+//  connection["connection"]["id"] = "Hotspot";
+//  connection["connection"]["uuid"] = QUuid::createUuid().toString().remove('{').remove('}');
+//  connection["connection"]["type"] = "802-11-wireless";
+//  connection["connection"]["interface-name"] = "wlan0";
+//  connection["connection"]["autoconnect"] = false;
+//
+//  connection["802-11-wireless"]["band"] = "bg";
+//  connection["802-11-wireless"]["mode"] = "ap";
+//  connection["802-11-wireless"]["ssid"] = tethering_ssid.toUtf8();
+//
+//  connection["802-11-wireless-security"]["group"] = QStringList("ccmp");
+//  connection["802-11-wireless-security"]["key-mgmt"] = "wpa-psk";
+//  connection["802-11-wireless-security"]["pairwise"] = QStringList("ccmp");
+//  connection["802-11-wireless-security"]["proto"] = QStringList("rsn");
+//  connection["802-11-wireless-security"]["psk"] = defaultTetheringPassword;
+//
+//  connection["ipv4"]["method"] = "shared";
+//  QMap<QString,QVariant> address;
+//  address["address"] = "192.168.43.1";
+//  address["prefix"] = 24u;
+//  connection["ipv4"]["address-data"] = QVariant::fromValue(IpConfig() << address);
+//  connection["ipv4"]["gateway"] = "192.168.43.1";
+//  connection["ipv4"]["route-metric"] = 1100;
+//  connection["ipv6"]["method"] = "ignore";
+//
+//  QDBusInterface nm_settings(NM_DBUS_SERVICE, NM_DBUS_PATH_SETTINGS, NM_DBUS_INTERFACE_SETTINGS, bus);
+//  nm_settings.setTimeout(DBUS_TIMEOUT);
+//  nm_settings.call("AddConnection", QVariant::fromValue(connection));
+//}
